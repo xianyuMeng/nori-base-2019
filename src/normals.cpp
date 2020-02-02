628c887c43deb090379be934f205e1f6c9137ce3
Hsien-Yu Meng <mengxy19@umd.edu>
2020-02-02 07:24:29 -0500

    init

---
+++ src/normals.cpp
@@ -0,0 +1,27 @@
+#include <nori/integrator.h>
+NORI_NAMESPACE_BEGIN
+class NormalIntegrator : public Integrator {
+    public:
+    NormalIntegrator(const PropertyList& props)
+    {
+        m_myproperty = props.getString('myProperty');
+        std::cout << "Para values = " << m_my_property << "\n";
+    }
+
+    Color3f Li(const Scene* scene, Sampler* sampler, const Ray3f& ray) const
+    {
+        return Color3f(0, 1, 0);
+    }
+    std::string toString() const
+    {
+        return tfm::format(
+            "NormalIntegrator[\n"
+            " myProperty = \"%s\"\n"
+            "]",
+            m_myproperty
+        );
+    }
+    protected:
+    std::string m_myproperty;
+}
+
