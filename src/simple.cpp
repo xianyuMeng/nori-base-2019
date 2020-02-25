03049f00a876ac815ed2449c5d77afef0e2c9ff3
Hsien-Yu Meng <mengxy19@umd.edu>
2020-02-25 13:49:12 -0500

    fix typo

--- src/simple.cpp
+++ src/simple.cpp
@@ -20,14 +20,14 @@ class SimpleIntegrator : public Integrator
         {
             return Color3f(0, 0, 0);
         }
-        Vector3f normal = its.shFrame.n.cwiseAbs();
+        Vector3f normal = its.shFrame.n;
         Point3f p = this->myPoint;
         Point3f x = its.p;
         Point3f diff = p - x;
         float norm_diff = diff.x() * diff.x() + diff.y() * diff.y() + diff.z() * diff.z();
         diff = diff / std::sqrt(norm_diff);
         float cos = (normal.x() * diff.x() + normal.y() * diff.y() + normal.z() * diff.z());
-        Ray3f vis(x, diff, 0, (p.x() - x.x()) / diff.x());
+        Ray3f vis(x, diff);
         if(scene->rayIntersect(vis))
         {
             return Color3f(0, 0, 0);
