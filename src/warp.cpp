cc88289bb2adf4e13a02d0f9d78c537bc9430ce9
Hsien-Yu Meng <mengxy19@umd.edu>
2020-02-28 15:41:12 -0500

    sample

--- src/warp.cpp
+++ src/warp.cpp
@@ -39,35 +39,92 @@ float Warp::squareToTentPdf(const Point2f &p) {
 }
 
 Point2f Warp::squareToUniformDisk(const Point2f &sample) {
-    throw NoriException("Warp::squareToUniformDisk() is not yet implemented!");
+    float theta = 2 * M_PI * sample.y();
+    float r = std::sqrt(sample.x());
+    return Point2f(r * std::cos(theta), r * std::sin(theta));
+    //throw NoriException("Warp::squareToUniformDisk() is not yet implemented!");
 }
 
 float Warp::squareToUniformDiskPdf(const Point2f &p) {
-    throw NoriException("Warp::squareToUniformDiskPdf() is not yet implemented!");
+    return (p.x() * p.x() + p.y() * p.y() < 1 ? 1.0f / M_PI : 0.0f);
+    //throw NoriException("Warp::squareToUniformDiskPdf() is not yet implemented!");
 }
 
+/*
+Reference:
+http://www.pbr-book.org/3ed-2018/Monte_Carlo_Integration/2D_Sampling_with_Multidimensional_Transformations.html
+*/
+
 Vector3f Warp::squareToUniformSphere(const Point2f &sample) {
-    throw NoriException("Warp::squareToUniformSphere() is not yet implemented!");
+    //throw NoriException("Warp::squareToUniformSphere() is not yet implemented!");
+    float z = 1 - 2 * sample.x();
+    float phi = 2 * M_PI * sample.y();
+    return Vector3f(
+        std::cos(phi) * std::sqrt(1 - z * z),
+        std::sin(phi) * std::sqrt(1 - z * z),
+        z
+    );
 }
 
 float Warp::squareToUniformSpherePdf(const Vector3f &v) {
-    throw NoriException("Warp::squareToUniformSpherePdf() is not yet implemented!");
+    //throw NoriException("Warp::squareToUniformSpherePdf() is not yet implemented!");
+    float r = std::sqrt(v.x() * v.x() + v.y() * v.y() + v.z() * v.z());
+    return (r > 1.0f) ? 0.0f : 1.0f / (4 * M_PI);
 }
 
 Vector3f Warp::squareToUniformHemisphere(const Point2f &sample) {
-    throw NoriException("Warp::squareToUniformHemisphere() is not yet implemented!");
+    //throw NoriException("Warp::squareToUniformHemisphere() is not yet implemented!");
+    float theta = std::acos(1 - sample.x());
+    float phi = 2 * M_PI * sample.y();
+    return Vector3f(
+        std::sin(theta) * std::cos(phi),
+        std::sin(theta) * std::sin(phi),
+        1 - sample.x()
+    );
 }
 
 float Warp::squareToUniformHemispherePdf(const Vector3f &v) {
-    throw NoriException("Warp::squareToUniformHemispherePdf() is not yet implemented!");
+    //throw NoriException("Warp::squareToUniformHemispherePdf() is not yet implemented!");
+    float theta = std::acos(v.z());
+    float r = std::sqrt(v.x() * v.x() + v.y() * v.y() + v.z() * v.z());
+    return (r > 1.0f || theta > M_PI / 2.0f || theta < 0.0f) ? 0.0f : 1.0f / (2 * M_PI);
 }
 
 Vector3f Warp::squareToCosineHemisphere(const Point2f &sample) {
-    throw NoriException("Warp::squareToCosineHemisphere() is not yet implemented!");
+    //throw NoriException("Warp::squareToCosineHemisphere() is not yet implemented!");
+//    float phi = 2 * M_PI * sample.y();
+//    float theta = std::asin(sample.x()) / 2.0f;
+//    return Vector3f(
+//        std::sin(theta) * std::cos(phi),
+//        std::sin(theta) * std::sin(phi),
+//        std::cos(theta)
+//        );
+        float r1 = 2.0f * sample.x() - 1.0f;
+		float r2 = 2.0f * sample.y() - 1.0f;
+		float phi, r;
+		if (r1 == 0 && r2 == 0) {
+			r = phi = 0;
+		}
+		else if (r1 * r1 > r2 * r2) {
+			r = r1;
+			phi = (M_PI / 4.0f) * (r2 / r1);
+		}
+		else {
+			r = r2;
+			phi = (M_PI / 2.0f) - (r1 / r2) * (M_PI / 4.0f);
+		}
+		float cosPhi = std::cos(phi);
+		float sinPhi = std::sin(phi);
+		Vector2f t = Vector2f(r * cosPhi, r * sinPhi);
+		float z = std::sqrt(std::max(0.0f, 1.0f - t.x()*t.x() - t.y()*t.y()));
+		return Vector3f(t.x(), t.y(), z);
 }
 
 float Warp::squareToCosineHemispherePdf(const Vector3f &v) {
-    throw NoriException("Warp::squareToCosineHemispherePdf() is not yet implemented!");
+    //throw NoriException("Warp::squareToCosineHemispherePdf() is not yet implemented!");
+    float theta = std::acos(v.z());
+    float r = std::sqrt(v.x() * v.x() + v.y() * v.y() + v.z() * v.z());
+    return (theta > M_PI / 2.0f || theta < 0.0f || r > 1.0f) ? 0.0f : std::cos(theta) / M_PI;
 }
 
 Vector3f Warp::squareToBeckmann(const Point2f &sample, float alpha) {
